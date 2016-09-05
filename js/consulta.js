a7ec566716d875a1bfad1442b9acc6e59afdbf1d
tgispert <spfury@gmail.com>
2016-09-05 00:10:32 -0300

    js de consulta

--- js/consulta.js
+++ js/consulta.js
@@ -1,8 +1,11 @@
 function sendMail() {
     var link = "mailto:tomasgispert@gmail.com"
              + "?cc=consulta@lacasetaplaya.com"
-             + "&subject=" + escape("This is my subject")
-             + "&body=" + escape(document.getElementById('myText').value)
+             + "&subject=" + escape("Consulta web")
+             + "&nombre=" + escape(document.getElementById('InputName').value)
+             + "&telefono=" + escape(document.getElementById('InputPhone').value)
+             + "&email=" + escape(document.getElementById('InputEmail').value)
+             + "&consulta=" + escape(document.getElementById('InputMessage').value)
     ;
 
     window.location.href = link;

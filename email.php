23d29e5163290f69f8372c243f606bfd59858883
tgispert <tomasgispert@gmail.com>
2016-09-07 16:51:35 -0300

    mail de consulta: info@lacasetaplaya.com

--- email.php
+++ email.php
@@ -6,7 +6,7 @@ if($_POST){
     $email = $_REQUEST['InputEmail'];
     $message = $_REQUEST['InputMessage'];
 
-    $to = "tomasgispert@gmail.com";
+    $to = "info@lacasetaplaya.com";
 
     $mensaje = 
 		"Nombre: " .$name ."\r\n"
@@ -17,10 +17,10 @@ if($_POST){
 
 	$subject = "La Caseta Playa - Consulta de: " .$email;
 
-	$header = "From: " .$email ."\r\n"; 
-	$header.= "MIME-Version: 1.0\r\n"; 
-	$header.= "Content-Type: text/plain; charset=utf-8\r\n"; 
-	$header.= "X-Priority: 1\r\n"; 
+	$header = "From: " .$email ."\r\n";
+	$header.= "MIME-Version: 1.0\r\n";
+	$header.= "Content-Type: text/plain; charset=utf-8\r\n";
+	$header.= "X-Priority: 1\r\n";
 
     mail($to,$subject,$mensaje,$header);
 }
